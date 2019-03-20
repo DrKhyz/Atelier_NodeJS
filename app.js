19d49c4023d968182e998ae5813281c5dae8bb34
Jonathan Galvao Diniz <jo.diniz@live.fr>
2019-03-20 16:59:50 +0100

    App.js added

---
+++ app.js
@@ -0,0 +1,6 @@
+const chalk = require('chalk');
+
+console.log(chalk.magenta('Stephane'));
+console.log(chalk.red('Cloé'));
+console.log(chalk.yellow('Sylène'));
+console.log(chalk.green('Marlène'));
